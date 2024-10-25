03904b37c3f238871c745035115955af8a182c02
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-10-25 19:43:26 +0530

    feat : pwa configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,      // Enable React strict mode for improved error handling
+    swcMinify: true,            // Enable SWC minification for improved performance
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
+    },
+    images: {
+      domains: ["res.cloudinary.com"],
+      remotePatterns: [{
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        
+      },],
+    },
+};
+
+export default withPWA({
+    dest: "public",         // destination directory for the PWA files
+          // disable PWA in the development environment
+    register: true,         // register the PWA service worker
+    skipWaiting: true,      // skip waiting for service worker activation
+})(nextConfig);
--- src/app/layout.js
+++ src/app/layout.js
@@ -14,16 +14,17 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "moonshot-email-client-",
-  description: "The app features an email list page. This page shows the list of emails sent to a user.Clicking on any email item in the list should split the screen into a master-slave (left-right) screen type where the master (left) shows the email list (with the selected email item) while the slave (right) shows the body of the email. The body of the email is not known ahead of time and should be loaded only when the email item is clicked"
-
+  title: "🚀 Mail",
+  description: "🚀 Mail",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
      <ReduxProvider>
-     <head><link rel="icon" href="/favicon.ico" sizes="any" /></head>
+     <head><link rel="icon" href="/icons-256.png" sizes="any" /></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
